cf998ff32dc5c11b361378c62924447d095971a9
CeddlyBurge <ceddlyburge@gmail.com>
2017-11-17 08:54:15 +0000

    Add some functions to work with the enemies Queue

--- src/elm/Updates/Master.elm
+++ src/elm/Updates/Master.elm
@@ -3,7 +3,7 @@ module Updates.Master exposing (..)
 import Models.Models exposing (..)
 import Queue exposing (..)
 import Html exposing (Html, text)
-import Time exposing (Time)
+import Time exposing (Time, inSeconds)
 import Keyboard exposing (KeyCode)
 import Updates.Actions exposing (curryActions)
 import Views.GameGrid exposing (..)
@@ -22,8 +22,8 @@ updateWithoutCurrying msg model =
         actions = curryActions model
     in
         case msg of
-            TimeUpdate dt ->
-                ( model, Cmd.none )
+            TimeUpdate time ->
+                (timeUpdate (inSeconds time) model, Cmd.none)    
 
             KeyDown keyCode ->
                 ( keyDown keyCode model actions, Cmd.none )
@@ -44,6 +44,88 @@ initialModel =
     , enemyGridElement = (\e -> Html.p[][]) 
     }
 
+-- update energy of first enemies
+-- get first enemy from Queue
+-- work out where it wants to move
+-- it if can move then do so
+-- put it back on the queue
+-- check for gameover
+
+-- can everything to work with a maybe
+-- dequeue enemy, which is a maybe
+-- desired position works with an actual position, just return a nothing in no position
+-- if occupied takes a maybe position and returns a maybe position
+-- moveEnemy function takes the maybe and returns a modified enemy if there is a position, or returns the original enemey otherwise
+-- Maybe.map, Maybe.andThen
+
+-- deque enemy
+-- run move enemy
+-- run energise enemy
+
+
+timeUpdate : Float -> Model -> Model
+timeUpdate milliseconds model  =
+    model
+    --let
+        --enemy = (Queue.deq model.enemies)
+        --|> Maybe.andThen (justUpdateEnemyEnergy milliseconds)
+        --desiredPosition = desiredEnemyPosition enemy.position model.position
+    --in
+        --enemy = (Queue.deq model.enemies)
+        --|> Maybe.andThen (justUpdateEnemyEnergy milliseconds)
+        --model
+        --{ model | enemies = Queue.enq enemy }
+
+-- there is probably a good way to pass Queue.enq to list.map to create a queue from a list
+-- timeUpdate : Float -> Model -> Model
+-- timeUpdate milliseconds model  =
+--     { model | enemies = List.map (updateEnemyEnergy milliseconds) model.enemies }
+
+processTopOfQueueAndReturnToQueue : Queue a -> (a -> a) -> Queue a
+processTopOfQueueAndReturnToQueue queue processor =
+    let
+      (maybeItem, list) = Queue.deq queue
+    in
+      case maybeItem of
+        Just item ->
+            Queue.enq (processor item) list 
+        Nothing ->
+            list
+
+
+updateEnemyEnergy2 : Float -> Model -> Maybe Enemy
+updateEnemyEnergy2 milliseconds model =
+    let
+      (enemy, enemies) = Queue.deq model.enemies
+    in
+        Maybe.andThen (justUpdateEnemyEnergy milliseconds) enemy
+
+justUpdateEnemyEnergy : Float -> Enemy -> Maybe Enemy
+justUpdateEnemyEnergy milliseconds enemy =
+    Just { enemy | energy = enemy.energy + milliseconds }
+
+updateEnemyEnergy : Float -> Enemy -> Enemy
+updateEnemyEnergy milliseconds enemy =
+    { enemy | energy = enemy.energy + milliseconds }
+
+desiredEnemyPosition : Position -> Position -> Position
+desiredEnemyPosition enemyPosition playerPosition =
+    Position 
+        (moveTowards enemyPosition.x playerPosition.x)
+        (moveTowards enemyPosition.y playerPosition.y)
+
+moveTowards : Int -> Int -> Int
+moveTowards current target =
+    if current > target then
+        current - 1
+    else if current < target then
+        current + 1
+    else
+        current
+
+isOccupied : Position -> List Enemy -> Bool
+isOccupied position enemies =
+    (List.any (\e -> (e.position.x == position.x) && (e.position.y == position.y)) enemies)
 
 
 keyDown : KeyCode -> Model -> Actions -> Model
