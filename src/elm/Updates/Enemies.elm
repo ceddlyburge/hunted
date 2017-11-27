7dba0c718b2f48bd4c3b98689d3de2184394d8b5
CeddlyBurge <ceddlyburge@gmail.com>
2017-11-27 22:04:57 +0000

    reorder functions

--- src/elm/Updates/Enemies.elm
+++ src/elm/Updates/Enemies.elm
@@ -26,50 +26,9 @@ mapf : (EnemyUpdate -> Enemy) -> (EnemyUpdate -> EnemyUpdate)
 mapf enemyFunction =
     (\enemyUpdate -> { enemyUpdate | enemy = enemyFunction enemyUpdate })
 
-initialEnemyUpdate : Enemy -> EnemyUpdate
-initialEnemyUpdate enemy =
-    EnemyUpdate enemy enemy.position (Position -1 -1) -- these -1s are bad, should use a maybe instead
-
-updateEnemy2 : Model -> Float -> (Enemy -> Enemy)
-updateEnemy2 model milliseconds =
-    initialEnemyUpdate
-    >> fmap (increaseEnemyEnergy milliseconds)
-    >> desiredEnemyPosition2 model.position
-    >> mapf (moveEnemyIfPossible model.enemies)
-    >> mapf resetEnergyIfMoved
-    >> returnEnemy
-
-
-desiredEnemyPosition2 : Position -> EnemyUpdate -> EnemyUpdate
-desiredEnemyPosition2 playerPosition enemyUpdate =
-    { enemyUpdate | desiredPosition = desiredEnemyPosition enemyUpdate.enemy.position playerPosition}
-
-moveEnemyIfPossible : Queue Enemy -> EnemyUpdate -> Enemy
-moveEnemyIfPossible enemies enemyUpdate =
-    if (canEnemyMove enemyUpdate.enemy enemyUpdate.desiredPosition enemies ) then
-        moveEnemy enemyUpdate.enemy enemyUpdate.desiredPosition 
-    else
-        enemyUpdate.enemy
-
-moveEnemy : Enemy -> Position -> Enemy 
-moveEnemy enemy newPosition =
-        { enemy | position = newPosition }
-
-resetEnergyIfMoved : EnemyUpdate -> Enemy
-resetEnergyIfMoved  enemyUpdate =
-   if (enemyUpdate.enemy.position == enemyUpdate.originalPosition) then
-       enemyUpdate.enemy
-   else
-       resetEnergy enemyUpdate.enemy
-
-resetEnergy : Enemy -> Enemy
-resetEnergy  enemy =
-    { enemy | energy = 0 }
-
-returnEnemy : EnemyUpdate -> Enemy
-returnEnemy  enemyUpdate =
-    enemyUpdate.enemy
-
+updateEnemies :  Float -> Model -> Model
+updateEnemies milliseconds model = 
+    { model | enemies = processTopOfQueueAndReturnToQueue model.enemies (updateEnemy2 model milliseconds) }
 
 processTopOfQueueAndReturnToQueue : Queue a -> (a -> a) -> Queue a
 processTopOfQueueAndReturnToQueue queue processor =
@@ -82,40 +41,66 @@ processTopOfQueueAndReturnToQueue queue processor =
         Nothing ->
             list
 
-updateEnemies :  Float -> Model -> Model
-updateEnemies milliseconds model = 
-    { model | enemies = processTopOfQueueAndReturnToQueue model.enemies (updateEnemy2 model milliseconds) }
+updateEnemy2 : Model -> Float -> (Enemy -> Enemy)
+updateEnemy2 model milliseconds =
+    initialEnemyUpdate
+    >> fmap (increaseEnemyEnergy milliseconds)
+    >> desiredEnemyPosition2 model.position
+    >> mapf (moveEnemyIfPossible model.enemies)
+    >> mapf resetEnergyIfMoved
+    >> returnEnemy
 
-updateEnemy : Model -> Float -> (Enemy -> Enemy)
-updateEnemy model milliseconds =
-    increaseEnemyEnergy milliseconds
-    >> updateEnemyPositionAndEnergy model.enemies model.position
+initialEnemyUpdate : Enemy -> EnemyUpdate
+initialEnemyUpdate enemy =
+    EnemyUpdate enemy enemy.position (Position -1 -1) -- these -1s are bad, should use a maybe instead
 
 increaseEnemyEnergy : Float -> Enemy -> Enemy
 increaseEnemyEnergy milliseconds enemy =
     { enemy | energy = enemy.energy + milliseconds }
 
-updateEnemyPositionAndEnergy : Queue Enemy -> Position -> Enemy -> Enemy
-updateEnemyPositionAndEnergy  enemies playerPosition enemy =
-    let 
-        newPosition = desiredEnemyPosition enemy.position playerPosition
-    in
-        if (canEnemyMove enemy newPosition enemies ) then
-            { enemy |
-                position = newPosition 
-                , energy = 0 }
-        else
-            enemy
+desiredEnemyPosition2 : Position -> EnemyUpdate -> EnemyUpdate
+desiredEnemyPosition2 playerPosition enemyUpdate =
+    { enemyUpdate | desiredPosition = desiredEnemyPosition enemyUpdate.enemy.position playerPosition}
 
-canEnemyMove : Enemy -> Position -> Queue Enemy -> Bool
-canEnemyMove enemy desiredPosition enemies =
-    (enemy.energy >= 2) && (isOccupiedByEnemy desiredPosition enemies == False)
+moveEnemyIfPossible : Queue Enemy -> EnemyUpdate -> Enemy
+moveEnemyIfPossible enemies enemyUpdate =
+    if (canEnemyMove enemyUpdate.enemy enemyUpdate.desiredPosition enemies ) then
+        moveEnemy enemyUpdate.enemy enemyUpdate.desiredPosition 
+    else
+        enemyUpdate.enemy
 
+resetEnergyIfMoved : EnemyUpdate -> Enemy
+resetEnergyIfMoved  enemyUpdate =
+   if (enemyUpdate.enemy.position == enemyUpdate.originalPosition) then
+       enemyUpdate.enemy
+   else
+       resetEnergy enemyUpdate.enemy
+
+returnEnemy : EnemyUpdate -> Enemy
+returnEnemy  enemyUpdate =
+    enemyUpdate.enemy
+
+
+-- level 2
 desiredEnemyPosition : Position -> Position -> Position
 desiredEnemyPosition enemyPosition playerPosition =
     Position 
         (moveTowards enemyPosition.x playerPosition.x)
         (moveTowards enemyPosition.y playerPosition.y)
+
+canEnemyMove : Enemy -> Position -> Queue Enemy -> Bool
+canEnemyMove enemy desiredPosition enemies =
+    (enemy.energy >= 2) && (isOccupiedByEnemy desiredPosition enemies == False)
+
+moveEnemy : Enemy -> Position -> Enemy 
+moveEnemy enemy newPosition =
+        { enemy | position = newPosition }
+
+resetEnergy : Enemy -> Enemy
+resetEnergy  enemy =
+    { enemy | energy = 0 }
+
+--level 3
 
 moveTowards : Int -> Int -> Int
 moveTowards current target =
@@ -125,6 +110,9 @@ moveTowards current target =
         current + 1
     else
         current
+
+
+
 
 isOccupiedByEnemy : Position -> Queue Enemy -> Bool
 isOccupiedByEnemy position enemies =
@@ -137,3 +125,20 @@ enemyPositionEqual position enemy =
 positionsEqual : Position -> Position -> Bool
 positionsEqual position1 position2 =
     position1.x == position2.x && position1.y == position2.y
+
+-- updateEnemyPositionAndEnergy : Queue Enemy -> Position -> Enemy -> Enemy
+-- updateEnemyPositionAndEnergy  enemies playerPosition enemy =
+--     let 
+--         newPosition = desiredEnemyPosition enemy.position playerPosition
+--     in
+--         if (canEnemyMove enemy newPosition enemies ) then
+--             { enemy |
+--                 position = newPosition 
+--                 , energy = 0 }
+--         else
+--             enemy
+
+-- updateEnemy : Model -> Float -> (Enemy -> Enemy)
+-- updateEnemy model milliseconds =
+--     increaseEnemyEnergy milliseconds
+--     >> updateEnemyPositionAndEnergy model.enemies model.position
