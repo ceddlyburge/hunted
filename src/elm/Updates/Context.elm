f6f7d6bfab6a020ac269280810b66fd829d8e7cd
CeddlyBurge <ceddlyburge@gmail.com>
2017-11-28 17:56:15 +0000

    Create context class

--- src/elm/Updates/Context.elm
+++ src/elm/Updates/Context.elm
@@ -1,15 +1,10 @@
-module Updates.EnemyUpdate exposing (..)
+module Updates.Context exposing (..)
 
-import Models.Position exposing (..)
-import Models.Enemy exposing (..)
+type Context a b =
+    Value a
+    | ValueAndContext a b
 
-import Updates.Context exposing (..)
 
-type alias EnemyUpdate =
-    { enemy : Enemy
-    , originalPosition : Position
-    , desiredPosition : Position
-    }
 
 -- functors (fmap) take a function that takes a value and returns a value, returning a function that both takes and returns a value + context
 -- monads  (liftM) take a function that takes a value and returns a value + context, returning a function that both takes and return a value + context
@@ -17,21 +12,7 @@ type alias EnemyUpdate =
 -- no definition for something that takes a function that takes a value + context and returns a value, functions basically shouldn't take a context it seems
 
 -- (functor) work with a function that takes an enemy and returns an enemy (increaseEnergyFromTime)
-fmap : (Enemy -> Enemy) -> (EnemyUpdate -> EnemyUpdate)
-fmap enemyFunction =
-    (\enemyUpdate -> { enemyUpdate | enemy = enemyFunction enemyUpdate.enemy })
 
 -- (monad) work with a function that takes an enemy and returns an enemyUpdate (desiredPosition)
 
 -- (no defintion) work with a function that takes an enemyUpdate and returns an enemy (decreaseEnergyFromMovement)
-mapf : (EnemyUpdate -> Enemy) -> (EnemyUpdate -> EnemyUpdate)
-mapf enemyFunction =
-    (\enemyUpdate -> { enemyUpdate | enemy = enemyFunction enemyUpdate })
-
-initialEnemyUpdate : Enemy -> EnemyUpdate
-initialEnemyUpdate enemy =
-    EnemyUpdate enemy enemy.position (Position -1 -1) -- these -1s are bad, should use a maybe instead
-
-enemy : EnemyUpdate -> Enemy
-enemy  enemyUpdate =
-    enemyUpdate.enemy
