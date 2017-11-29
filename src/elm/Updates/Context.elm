30069c8887c72d3dd69be52119215186127d4b90
CeddlyBurge <ceddlyburge@gmail.com>
2017-11-29 07:56:42 +0000

    Make context a functor
    Not sure this idea is going to work out though

--- src/elm/Updates/Context.elm
+++ src/elm/Updates/Context.elm
@@ -1,9 +1,7 @@
 module Updates.Context exposing (..)
 
 type Context a b =
-    Value a
-    | ValueAndContext a b
-
+    ValueAndContext a b
 
 
 -- functors (fmap) take a function that takes a value and returns a value, returning a function that both takes and returns a value + context
@@ -11,7 +9,13 @@ type Context a b =
 -- applicatives (liftA) take a function + context, where the function takes a value and returns a value, returning a function that returns value + context
 -- no definition for something that takes a function that takes a value + context and returns a value, functions basically shouldn't take a context it seems
 
--- (functor) work with a function that takes an enemy and returns an enemy (increaseEnergyFromTime)
+-- (functor) work with a function that takes a value and returns a value
+fmap : (a -> a) -> (Context a b -> Context a b)
+fmap valueFunction =
+    (\context -> 
+        case context of
+            ValueAndContext a b ->
+                ValueAndContext (valueFunction a) b )
 
 -- (monad) work with a function that takes an enemy and returns an enemyUpdate (desiredPosition)
 
