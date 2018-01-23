b0706ee928b988fb197930e85b775b26a9b7d040
CeddlyBurge <ceddlyburge@gmail.com>
2018-01-23 07:50:02 +0000

    Don't show model state in ui now that the app is stable

--- src/elm/Views/Playing.elm
+++ src/elm/Views/Playing.elm
@@ -12,10 +12,10 @@ playing : Model -> Html Msg
 playing model =
     Html.div
         [ style [ ( "max-width", "400px" ), ( "min-width", "280px" ), ( "flex", "1" ) ] ]
-        [ Html.h1 [ style [ ( "font-size", "0.5em" ) ] ]
+        [ Html.h1 [ style [ ( "font-size", "0.5em" ), ( "display", "none" ) ] ]
             [ text (toString model) ]
         , svg [ version "1.1", viewBox "0 0 100 100" ]
-            (viewGridBackground model
+            (   viewGridBackground model
                 ++ [ model.playerGridElement ]
                 ++ viewEnemies model
             )
