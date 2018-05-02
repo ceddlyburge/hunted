69612326b99c30816521ced0abf371bbcd1ee395
CeddlyBurge <ceddlyburge@gmail.com>
2018-05-02 08:54:58 +0100

    Add some new types
    A key piece of learning for me here was that types cannot contain type alias'

--- src/elm/Updates/EnemyUpdate.elm
+++ src/elm/Updates/EnemyUpdate.elm
@@ -4,6 +4,27 @@ import Models.Position exposing (..)
 import Models.Enemy exposing (..)
 
 
+-- we can't use a type alias within a type, I think this was the thing that was confusing me.
+-- types don't have to be union types, they can just be a simple type
+-- maybe make a pr to the repo with this.
+type Position2 
+    = Position2 Int Int
+
+type OriginalAndDesiredPosition 
+    = OriginalAndDesiredPosition Position2 Position2
+
+type WithOriginalAndDesiredPosition a
+    = WithOriginalAndDesiredPosition a OriginalAndDesiredPosition
+
+type ValueAndContext valueType contextType
+    = ValueAndContext valueType contextType
+
+type alias EnemyUpdate3 a =
+    { enemy : a
+    , originalPosition : Position
+    , desiredPosition : Position
+    }
+
 type alias EnemyUpdate =
     { enemy : Enemy
     , originalPosition : Position
--- tests/EnemyUpdateTests.elm
+++ tests/EnemyUpdateTests.elm
@@ -8,6 +8,7 @@ import Models.Position exposing (..)
 import Updates.EnemyUpdate exposing (..)
 
 -- map takes an enemy -> enemy function, and returns a enemyUpdate -> enemyUpdate function. The returned function applies the passed function to the enemy parameter if enemyUpdate
+-- this isn't a functor I think, as a functor would be able to work with a function that takes a value and returns anything, whereas this requires that it return an enemy.
 mapTest : Test
 mapTest  =
     describe "map"
@@ -23,6 +24,24 @@ mapTest  =
                 |> Expect.equal (EnemyUpdate (enemyFunction enemyWithAllZeros) position position)
         ]
     
+-- mapReturn takes an enemyupdate -> enemy function, and returns a enemyUpdate -> enemyUpdate function. The returned function applies the passed function to the enemy parameter if enemyUpdate
+-- what is this in functional language, if anything.
+-- EnemyUpdate is the container, and Enemy is the simple value inside it
+-- function takes a container and returns a simple value
+-- mapTest : Test
+-- mapTest  =
+--     describe "map"
+--         [ fuzz3 float int int "map applies function to Enemy and leave positions unchanged" <|
+--             \energy x y ->
+--                 let
+--                     enemyFunction = \enemy -> { enemy | energy = energy}
+--                     position = Position x y
+--                 in
+--                 enemyWithAllZeros
+--                 |> \enemy -> EnemyUpdate enemy position position
+--                 |> Updates.EnemyUpdate.map enemyFunction
+--                 |> Expect.equal (EnemyUpdate (enemyFunction enemyWithAllZeros) position position)
+--        ]
 
 enemyWithAllZeros : Enemy
 enemyWithAllZeros = Enemy (Position 0 0) 0
