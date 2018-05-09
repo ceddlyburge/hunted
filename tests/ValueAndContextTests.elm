a8d25e7fe4c1e3002331b2b13c2e60cbf8bf7980
CeddlyBurge <ceddlyburge@gmail.com>
2018-05-09 08:29:07 +0100

    Add test for first functor law for ValueAndContext

--- tests/ValueAndContextTests.elm
+++ tests/ValueAndContextTests.elm
@@ -6,8 +6,8 @@ import Expect
 import Updates.ValueAndContext exposing (..)
 
 -- map takes an Value -> Value function, and returns a (ValueAndContext Value a)-> (ValueAndContext Value a) function. The returned function applies the passed function to the value parameter of the ValueAndContext
-valueAndContextMapTest : Test
-valueAndContextMapTest  =
+valueAndContextTest : Test
+valueAndContextTest  =
     describe "map"
         [ fuzz2 float string "map applies function to value and leaves context unchanged" <|
             \value context ->
@@ -15,7 +15,18 @@ valueAndContextMapTest  =
                     functionToMap = \x -> x / 2
                 in
                     ValueAndContext value context
-                    |> Updates.ValueAndContext.map functionToMap
+                    |> map functionToMap
                     |> Expect.equal (ValueAndContext (functionToMap value) context)
+        ,fuzz2 float string "mapping identity equals identity (functor law)" <|
+            \value context ->
+                let 
+                    valueAndContext = ValueAndContext value context    
+                in
+                    valueAndContext
+                    |> map identity
+                    |> Expect.equal (identity(valueAndContext))
         ]
 
+identity : a -> a
+identity x = 
+    x
